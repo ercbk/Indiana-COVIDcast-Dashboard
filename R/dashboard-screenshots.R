792a49edd6c1c4910de52611e68816d1a4068170
ercbk <ericbookax0@yahoo.com>
2020-10-11 20:33:42 -0400

    dash-screenshots - added gc code for old pngs

--- R/dashboard-screenshots.R
+++ R/dashboard-screenshots.R
@@ -40,6 +40,31 @@ webshot2::webshot(glue("{reacttab_path}"),
                  delay = 5)
 
 
+
+# garbage collection
+
+png_files <- tibble::tibble(paths = fs::dir_ls(glue::glue("{rprojroot::find_rstudio_root_file()}/images/dashboard"), regexp = "png")) %>% 
+   mutate(
+      chart = stringr::str_extract(paths,
+                                   pattern = "[a-z]*-[a-z]*-[a-z]*"),
+      date = stringr::str_extract(paths,
+                                  pattern = "[0-9]{4}-[0-9]{2}-[0-9]{2}") %>%
+         as.Date()
+   )
+
+# clean-up old pngs and extraneous html output
+png_files %>% 
+   group_by(chart) %>% 
+   add_count() %>% 
+   filter(n > 7) %>%
+   filter(date == min(date)) %>% 
+   pull(paths) %>% 
+   fs::file_delete(.)
+
+
+fs::file_delete(glue::glue("{rprojroot::find_rstudio_root_file()}/README.html"))
+
+
 # create path to artifacts
 dumbbell_files <- stringr::str_replace(dumbbell_path, ".html", "_files")
 reacttab_files <- stringr::str_replace(reacttab_path, ".html", "_files")
